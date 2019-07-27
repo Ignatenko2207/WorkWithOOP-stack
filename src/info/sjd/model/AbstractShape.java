b989136834951b4788d863e49998f444a0b3e5ea
Alex <ignatenko2207@gmail.com>
2019-07-27 22:11:07 +0300

    Remove unused code

--- src/info/sjd/model/AbstractShape.java
+++ src/info/sjd/model/AbstractShape.java
@@ -2,10 +2,4 @@ package info.sjd.model;
 
 public abstract class AbstractShape implements Shape{
 
-    private String className = "Abstract shape";
-
-    @Override
-    public String toString() {
-        return className;
-    }
 }
